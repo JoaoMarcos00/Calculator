7c853c93826b444559395ae2b91c2f03808c0b34
João Marcos <86451674+JoaoMarcos00@users.noreply.github.com>
2023-02-27 10:26:35 -0300

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -6,53 +6,38 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            double num1, num2, result;
-            char op;
+            Console.WriteLine("Welcome to the calculator!");
+            Console.WriteLine("Enter a number: ");
+            double num1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o primeiro número:");
-            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter an operation (+, -, *, /): ");
+            char operation = char.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o segundo número:");
-            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number: ");
+            double num2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite a operação (+, -, *, /):");
-            op = char.Parse(Console.ReadLine());
+            double result = 0;
 
-            switch (op)
+            switch (operation)
             {
                 case '+':
                     result = num1 + num2;
-                    Console.WriteLine($"O resultado de {num1} + {num2} é {result}");
                     break;
-
                 case '-':
                     result = num1 - num2;
-                    Console.WriteLine($"O resultado de {num1} - {num2} é {result}");
                     break;
-
                 case '*':
                     result = num1 * num2;
-                    Console.WriteLine($"O resultado de {num1} * {num2} é {result}");
                     break;
-
                 case '/':
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                        Console.WriteLine($"O resultado de {num1} / {num2} é {result}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Não é possível dividir por zero!");
-                    }
+                    result = num1 / num2;
                     break;
-
                 default:
-                    Console.WriteLine("Operação inválida!");
+                    Console.WriteLine("Invalid operation!");
                     break;
             }
 
-            Console.ReadKey();
+            Console.WriteLine("The result is: " + result);
         }
     }
 }
